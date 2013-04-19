c904fa3bb5b20d1a87b2dfa3a46f3b722a5543cd
Tomohisa Igarashi <tm.igarashi@gmail.com>
2013-04-19 17:35:45 +0900

    SWITCHYARD-1431 TransactionHandler not triggered in fault path

--- runtime/src/main/java/org/switchyard/handlers/TransactionHandler.java
+++ runtime/src/main/java/org/switchyard/handlers/TransactionHandler.java
@@ -41,6 +41,10 @@ import org.switchyard.policy.TransactionPolicy;
  */
 public class TransactionHandler implements ExchangeHandler {
     
+    /** A boolean flag indicates if the transaction should be rolled back or not on fault. true by default. */
+    public static final String ROLLBACK_ON_FAULT_PROPERTY =
+            "org.switchyard.exchange.transaction.rollbackOnFault";
+    
     private static final String JNDI_TRANSACTION_MANAGER = "java:jboss/TransactionManager";
     private static final String SUSPENDED_TRANSACTION_PROPERTY = 
             "org.switchyard.exchange.transaction.suspended";
@@ -79,6 +83,7 @@ public class TransactionHandler implements ExchangeHandler {
             handleAfter(exchange);
         } else {
             exchange.getContext().setProperty(BEFORE_INVOKED_PROPERTY, Boolean.TRUE).addLabels(BehaviorLabel.TRANSIENT.label());
+            exchange.getContext().setProperty(BEFORE_INVOKED_PROPERTY, Boolean.TRUE).addLabels(BehaviorLabel.TRANSIENT.label());
             handleBefore(exchange);
         }
     }
@@ -86,10 +91,12 @@ public class TransactionHandler implements ExchangeHandler {
     @Override
     public void handleFault(Exchange exchange) {
         try {
-            Transaction transaction = (Transaction)
-                    exchange.getContext().getPropertyValue(INITIATED_TRANSACTION_PROPERTY);
-            if (transaction != null) {
-                transaction.setRollbackOnly();
+            Property rollbackOnFault = exchange.getContext().getProperty(ROLLBACK_ON_FAULT_PROPERTY);
+            if (rollbackOnFault == null || rollbackOnFault.getValue() == null || Boolean.class.cast(rollbackOnFault.getValue())) {
+                Transaction transaction = getCurrentTransaction();
+                if (transaction != null) {
+                    transaction.setRollbackOnly();
+                }
             }
             handleAfter(exchange);
         } catch (Exception e) {
